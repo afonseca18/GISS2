71f6ec8ad05ca3ce8a2ea539194ea8829b5b757c
afonseca18 <38769721+afonseca18@users.noreply.github.com>
2018-05-26 19:48:15 +0100

    Add files via upload
    codigo java

---
+++ src/projecto/Projecto.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projecto;
+
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ *
+ * @author Batista
+ */
+public class Projecto {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        EcraInicial inicio = new EcraInicial();
+        inicio.setVisible(true);
+    }
+    
+}
