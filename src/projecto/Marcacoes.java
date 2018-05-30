e945d473512eb59467ca49a4e204fdfe14ff5a6e
Pedro.Batista <batped@hotmail.com>
2018-05-30 02:17:24 +0100

    app 1 concluida

--- src/projecto/Marcacoes.java
+++ src/projecto/Marcacoes.java
@@ -279,6 +279,7 @@ public class Marcacoes extends javax.swing.JFrame {
         recursosCB = new javax.swing.JComboBox<>();
         jLabel2 = new javax.swing.JLabel();
         materiaisCB = new javax.swing.JComboBox<>();
+        jButton3 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -360,6 +361,13 @@ public class Marcacoes extends javax.swing.JFrame {
 
         jLabel2.setText("Materiais");
 
+        jButton3.setText("Marcar");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -385,12 +393,12 @@ public class Marcacoes extends javax.swing.JFrame {
                         .addComponent(jLabel2)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                         .addComponent(materiaisCB, javax.swing.GroupLayout.PREFERRED_SIZE, 138, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(582, 582, 582))
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(jScrollPane1)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jButton1)
-                        .addGap(41, 41, 41))))
+                        .addGap(327, 327, 327)
+                        .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 150, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jScrollPane1))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jButton1)
+                .addGap(41, 41, 41))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -410,8 +418,9 @@ public class Marcacoes extends javax.swing.JFrame {
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                             .addComponent(salasCB)
                             .addComponent(jLabel2)
-                            .addComponent(materiaisCB, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))))
-                .addGap(18, 26, Short.MAX_VALUE)
+                            .addComponent(materiaisCB, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jButton3))))
+                .addGap(18, 23, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jButton2)
                     .addComponent(jButton1)
@@ -439,6 +448,17 @@ public class Marcacoes extends javax.swing.JFrame {
     private void colaboradoresCBActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_colaboradoresCBActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_colaboradoresCBActionPerformed
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+
+        try {
+            Marcar marcar = new Marcar(AreaClinicaID,con,datas[jTable1.getSelectedColumn()-1],jTable1.getSelectedRow());
+            marcar.setVisible(true);
+            this.setVisible(false);
+        } catch (SQLException ex) {
+            Logger.getLogger(Marcacoes.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }//GEN-LAST:event_jButton3ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -578,6 +598,7 @@ public class Marcacoes extends javax.swing.JFrame {
     private javax.swing.JLabel colaboradoresTV;
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JScrollPane jScrollPane1;
